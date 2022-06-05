38527d8407f84355b5a5a3c06d4f6e62cc717db3
antoine-agre <antoine.agre@free.fr>
2022-06-05 21:38:26 +0200

    config: set FILTER_FORWARD_ON_404 to true

--- src/main/java/com/example/crochet/JerseyConfiguration.java
+++ src/main/java/com/example/crochet/JerseyConfiguration.java
@@ -3,6 +3,7 @@ package com.example.crochet;
 import com.example.crochet.client.ClientResource;
 import com.example.crochet.commission.CommissionResource;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -15,5 +16,6 @@ public class JerseyConfiguration extends ResourceConfig {
 	public JerseyConfiguration() {
 		register(ClientResource.class);
 		register(CommissionResource.class);
+		property(ServletProperties.FILTER_FORWARD_ON_404, true);
 	}
 }
