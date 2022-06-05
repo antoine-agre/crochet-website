de9856dd2fb4fae1740bf83b4a101999aad10089
antoine-agre <antoine.agre@free.fr>
2022-06-05 21:37:40 +0200

    feat: add Commission POST and add idClient QueryParam for GET

--- src/main/java/com/example/crochet/commission/CommissionResource.java
+++ src/main/java/com/example/crochet/commission/CommissionResource.java
@@ -1,10 +1,10 @@
 package com.example.crochet.commission;
 
+import com.example.crochet.client.Client;
+import com.example.crochet.client.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +17,21 @@ public class CommissionResource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Commission> getListeComms(){
+	public List<Commission> getListeComms(@QueryParam("idClient") Long idClient){
+
 		ArrayList<Commission> out = new ArrayList<>();
-		commRepository.findAll().forEach(out :: add);
+		commRepository.findAll().forEach(out::add);
+
+		if(idClient != null) {
+			out.removeIf(c -> {return (c.getClient() == null || !c.getClient().getId().equals(idClient));});
+		}
+
 		return out;
 	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Commission createComm(Commission c){return commRepository.save(c);}
 
 }
