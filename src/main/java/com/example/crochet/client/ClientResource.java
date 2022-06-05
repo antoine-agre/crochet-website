4e086556d4c2cb18df4a63741641ac89b2cb5771
antoine-agre <antoine.agre@free.fr>
2022-06-06 00:23:42 +0200

    feat: add Client GET by id, DELETE, PATCH, and PUT

--- src/main/java/com/example/crochet/client/ClientResource.java
+++ src/main/java/com/example/crochet/client/ClientResource.java
@@ -1,5 +1,6 @@
 package com.example.crochet.client;
 
+import com.example.crochet.commission.Commission;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.print.attribute.standard.Media;
@@ -7,6 +8,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Path("clients")
 public class ClientResource {
@@ -26,5 +28,50 @@ public class ClientResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Client createClient(Client c){return clientRepository.save(c);}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Client getClient(@PathParam("id") Long id) {
+		Optional<Client> opt = clientRepository.findById(id);
+		return opt.orElse(null);
+	}
+
+	@DELETE
+	@Path("{id}")
+	public void deleteClient(@PathParam("id") Long id) {
+		clientRepository.deleteById(id);
+	}
+
+	@PATCH
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Client patchClient(@PathParam("id") Long id, Client c) {
+		Optional<Client> optionalClient = clientRepository.findById(id);
+		if(optionalClient.isPresent()){
+			Client out = optionalClient.get();
+
+			if(c.getNom() != null) {out.setNom(c.getNom());}
+			if(c.getContacts() != null) {out.setContacts(c.getContacts());}
+
+			//p.setAge(age);
+			clientRepository.save(out);
+			return out;
+		}
+		else {return null;}
+	}
+
+	@PUT
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Client putClient(@PathParam("id") Long id, Client cli) {
+		if(clientRepository.findById(id).isPresent()) {
+			cli.setId(id);
+			clientRepository.save(cli);
+			return cli;
+		}
+		else {return null;}
+	}
 
 }
